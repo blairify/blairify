bdb1a44a2d1a9a045cae3614ffb5c9413f1937d1
Mateusz Jakubowski <105046204+Jakubowski1@users.noreply.github.com>
2026-01-18 18:51:17 +0100

    feat: support enviroment dev

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getAppUrl() {
+  if (process.env.NEXT_PUBLIC_APP_URL) {
+    return process.env.NEXT_PUBLIC_APP_URL;
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    return process.env.NEXT_PUBLIC_APP_URL_LOCAL || "http://localhost:3000";
+  }
+
+  if (
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ||
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "development"
+  ) {
+    return (
+      process.env.NEXT_PUBLIC_APP_URL_DEV ||
+      (process.env.NEXT_PUBLIC_VERCEL_URL
+        ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+        : "https://blairify-git-payment-gate-blairifys-projects.vercel.app")
+    );
+  }
+
+  return process.env.NEXT_PUBLIC_APP_URL || "https://blairify.com";
+}
